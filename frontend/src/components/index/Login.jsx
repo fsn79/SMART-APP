7394b7d6da4690f4e5766168d4c5f04037d981d8
Alice <andreevaalisa42@gmail.com>
2021-08-20 17:50:29 +0300

    add login form

--- frontend/src/components/index/Login.jsx
+++ frontend/src/components/index/Login.jsx
@@ -1,9 +1,25 @@
 function Login() {
   // Форма авторизации
   return (
-    <form>
-      <h2>Login form</h2>
-    </form>
+  <div class="flex-direction--column formbg padding-horizontal--48">
+        <span class="padding-bottom--15">Sign in to your account</span>
+        <form id="login">
+          <div class="field padding-bottom--24">
+            <label for="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div class="field padding-bottom--24">
+            <div>
+              <label for="password">Password</label>
+            </div>
+            <input type="password" name="password" />
+          </div>
+          <div class="field padding-bottom--24">
+            <input type="submit" name="submit" value="Continue" className="button"/>
+          </div>
+        </form>
+  </div>
   );
 }
+
 export default Login;
