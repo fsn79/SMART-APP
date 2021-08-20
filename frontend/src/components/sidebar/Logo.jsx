76a11b749acb427146ae469ea68a347d1466e360
Anya <makarobaanya@yandex.ru>
2021-08-20 17:57:07 +0300

    html

--- frontend/src/components/sidebar/Logo.jsx
+++ frontend/src/components/sidebar/Logo.jsx
@@ -1,6 +1,6 @@
 function Logo() {
   // Боковая панель
-  return <div>LOGO</div>;
+  return <h1 id="logo"><a>SMART APP</a></h1>;
 }
 
 export default Logo;
--- frontend/src/components/sidebar/Navigation.jsx
+++ frontend/src/components/sidebar/Navigation.jsx
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 function Navigation() {
   // Боковая панель
   return (
-    <div>
-      <Link to='/create-work-center'>Create Work Center</Link>
-      <Link to='/edit-work-center'>Edit Work Center</Link>
-      <Link to='/create-user'>Create User</Link>
-      <Link to='/edit-user'>Edit User</Link>
-      <Link to='/create-item'>Create Item</Link>
-      <Link to='/edit-item'>Edit Item</Link>
-      <Link to='/create-order'>Create Order</Link>
-      <Link to='/orders'>Orders</Link>
-      <Link to='/logout'>Logout</Link>
-    </div>
+    <nav id="nav">
+       <ul >
+      <Link to='/edit-work-center'><li><a className='link'>Edit Work Center</a></li></Link>
+      <Link to='/create-user'><li><a className='link'>Create User</a></li></Link>
+      <Link to='/edit-user'><li><a className='link'>Edit User</a></li></Link>
+      <Link to='/create-item'><li><a className='link'>Create Item</a></li></Link>
+      <Link to='/edit-item'><li><a className='link'>Edit Item</a></li></Link>
+      <Link to='/create-order'><li><a className='link'>Create Order</a></li></Link>
+      <Link to='/orders'><li><a className='link' >Orders</a></li></Link>
+      <Link to='/logout'><li><a className='link'>Logout</a></li></Link>
+      </ul>
+    </nav>
   );
 }
 
