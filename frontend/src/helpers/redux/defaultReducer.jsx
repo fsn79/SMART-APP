ca779d8b02296afcf5605941dab152175b3536bd
Dmitry Rum <acopalypse@yandex.ru>
2021-08-20 14:52:16 +0300

    redux intermediate commit

---
+++ frontend/src/helpers/redux/defaultReducer.jsx
@@ -0,0 +1,13 @@
+const initState = {
+  load: false,
+  data: '',
+};
+
+function reducer(state = initState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
---
+++ frontend/src/helpers/redux/defaultSaga.jsx
@@ -0,0 +1,4 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { HOST } from './actionTypes.jsx';
+
+async function defaultFetch(HOST) {}
