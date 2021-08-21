8b71bb4ba642d0570c3f49e4bc60ce3ecba4d8e4
Anya <makarobaanya@yandex.ru>
2021-08-21 12:26:28 +0300

    createWork

--- frontend/src/components/manager/CreateOrder.jsx
+++ frontend/src/components/manager/CreateOrder.jsx
@@ -1,9 +1,43 @@
 function CreateOrder() {
   // Форма создания задачи
   return (
-    <form>
-      <h2>Create Order</h2>
-    </form>
+    <div class="flex-direction--column formbg padding-horizontal--48">
+    <span class="padding-bottom--15">New Work Order creation form</span>
+<form>
+  <div className='borderDiv'>
+    <p>Select item name or part number</p>
+   <div class="field padding-bottom--24">
+    <label for="itemName">Item Name</label>
+    <p><select>
+    <option></option>
+    <option></option>
+  </select></p>
+  </div>
+  <div class="field padding-bottom--24">
+    <label for="workOrder">Enter Work Order number or click autogenerate</label>
+    <input type="text" name="workOrder" />
+    </div>
+    </div>
+  <div class="field padding-bottom--24">
+    <label for="">Enter Work Order planned quantity</label>
+    <input type="number" name="" />
+  </div>
+  <div class="field padding-bottom--24">
+    <div class="grid--50-50">
+      <label for="promisedDate">Enter Work Order promised date</label>
+    </div>
+    <input type="date" name="promisedDate" />
+  </div>
+  <label for="productionPrioroty">Production Prioroty</label>
+  <p><select>
+    <option>Low</option>
+    <option>High</option>
+  </select></p>
+  <div class="field padding-bottom--24">
+    <input type="submit" name="submit" value="Create" />
+  </div>
+</form>
+</div>
   );
 }
 
