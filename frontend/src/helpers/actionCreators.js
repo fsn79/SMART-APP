709d47ed9b1e782a82eddaf0c3a10cbbd6439043
fsn79 <f_s_n@mail.ru>
2021-08-21 17:22:55 +0300

    add action types and controllers

---
+++ frontend/src/helpers/actionCreators.js
@@ -0,0 +1,42 @@
+import {
+  CREATE_USER,
+  CREATE_WORK_CENTER,
+  CREATE_ITEM,
+  CREATE_ORDER,
+  EDIT_USER,
+  EDIT_WORK_CENTER,
+  EDIT_ITEM,
+  EDIT_ORDER,
+} from './actionTypes';
+
+export function createUserAC(payload) {
+  return { type: CREATE_USER, payload };
+}
+
+export function createWorkCenterAC(payload) {
+  return { type: CREATE_WORK_CENTER, payload };
+}
+
+export function createItemAC(payload) {
+  return { type: CREATE_ITEM, payload };
+}
+
+export function createOrderAC(payload) {
+  return { type: CREATE_ORDER, payload };
+}
+
+export function editUserAC(payload) {
+  return { type: EDIT_USER, payload };
+}
+
+export function editWorkCenterAC(payload) {
+  return { type: EDIT_WORK_CENTER, payload };
+}
+
+export function editItemAC(payload) {
+  return { type: EDIT_ITEM, payload };
+}
+
+export function editOrderAC(payload) {
+  return { type: EDIT_ORDER, payload };
+}
