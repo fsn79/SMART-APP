cc4890d8597b5748a86c07d07c82c7adac42fd7a
Dmitry Rum <acopalypse@yandex.ru>
2021-08-23 11:39:59 +0300

    db fix

--- backend/models/employees.js
+++ backend/models/employees.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Employees extends Model {
     /**
@@ -12,56 +10,59 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Employees.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    lastname: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    firstname: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    email: {
-      allowNull: false,
-      unique: true,
-      type: DataTypes.STRING
-    },
-    password: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    jobtitle: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    workcenterid: {
-      allowNull: false,
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'workcenters',
-        key: 'id',
-        onUpdate: 'cascade',
-        onDelete: 'cascade'
+  }
+  Employees.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
       },
+      lastname: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      firstname: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      jobtitle: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      workcenterid: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Workcenters',
+          key: 'id',
+          onUpdate: 'cascade',
+          onDelete: 'cascade',
+        },
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
     },
-    status: {
-      allowNull: false,
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
+    {
+      sequelize,
+      freezeTableName: true,
+      modelName: 'Employees',
     },
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    freezeTableName: true,
-    modelName: 'employees',
-  });
+  );
   return Employees;
 };
