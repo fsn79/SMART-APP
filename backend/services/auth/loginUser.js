dcecea9ffadae57e35ed71573fb471de52172e08
Miroslav <gmpPB@yandex.ru>
2021-08-23 14:34:26 +0300

    add login in bakend

--- backend/services/auth/loginUser.js
+++ backend/services/auth/loginUser.js
@@ -1,8 +1,65 @@
 require('dotenv').config();
+const db = require('../../models');
+const bcrypt = require('bcrypt');
 
-function loginUser(req, res) {
-  return true
-};
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+  try {
+    const checkUser = await db.employees.findOne({
+      where: {
+        email
+      }
+    });
+  } catch (e) {
+    res.status(404).send(e)
+  }
+
+  if(checkUser !== null) {
+    try {
+      const checkPassword = await bcrypt.compare(password, checkUser.password);
+    } catch (e) {
+      res.status(404).send(e)
+    }
+
+    try {
+      if(checkUser && checkPassword) {
+        if(checkUser.jobtitle === 'admin') {
+          const nameOfUser = checkUser.name;
+          req.session.user = checkUser;
+          res.status(200).json({ masage: 'admin', checkUser });
+        };
+      }
+    } catch (e) {
+      res.status(404).send(e)
+    }
+
+    try {
+      if(checkUser.jobtitle === 'manager') {
+        const nameOfUser = checkUser.name;
+        req.session.user = checkUser;
+        res.status(200).json({ masage: 'manager', checkUser });
+      };
+    } catch (e) {
+      res.status(404).send(e)
+    }
+
+    try {
+      if(checkUser.jobtitle === 'worker') {
+        const nameOfUser = checkUser.name;
+        req.session.user = checkUser;
+        const listOrders = await db.orders.findAll({
+          limit: 10,
+          order: [['"priority" DESC'], ['"promiseddate" DESC']],
+          include: db.items,
+        });
+        res.status(200).json({ masage: 'worker', listOrders, checkUser });
+      };
+    } catch (e) {
+      res.status(404).send(e)
+    }
+
+    };
+  };
 
 module.exports = loginUser;
 
