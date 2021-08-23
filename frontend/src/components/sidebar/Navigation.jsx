e2961c088143845ed4d62c1d19b2499ce87c6fd1
Miroslav <gmpPB@yandex.ru>
2021-08-23 14:35:37 +0300

    add login in bakend

--- frontend/src/components/sidebar/Navigation.jsx
+++ frontend/src/components/sidebar/Navigation.jsx
@@ -4,16 +4,16 @@ function Navigation() {
   // Боковая панель
   return (
     <nav id="nav">
-       <ul >
-      <Link to='/edit-work-center'><li><a className='link'>Edit Work Center</a></li></Link>
-      <Link to='/create-user'><li><a className='link'>Create User</a></li></Link>
-      <Link to='/list-users'><li><a className='link'>Edit User</a></li></Link>
-      <Link to='/create-item'><li><a className='link'>Create Item</a></li></Link>
-      <Link to='/edit-item'><li><a className='link'>Edit Item</a></li></Link>
-      <Link to='/create-order'><li><a className='link'>Create Order</a></li></Link>
-      <Link to='/orders'><li><a className='link' >Orders</a></li></Link>
-      <Link to='/logout'><li><a className='link'>Logout</a></li></Link>
-      <Link to='/create-work-center'><li><a className='link'>Create work center</a></li></Link>
+      <ul >
+        <li><Link className='link' to='/edit-work-center'>Edit Work Center</Link></li>
+        <li><Link className='link' to='/create-user'>Create User</Link></li>
+        <li><Link className='link' to='/list-users'>Edit User</Link></li>
+        <li><Link className='link' to='/create-item'>Create Item</Link></li>
+        <li><Link className='link' to='/edit-item'>Edit Item</Link></li>
+        <li><Link className='link' to='/create-order'>Create Order</Link></li>
+        <li><Link className='link' to='/orders'>Orders</Link></li>
+        <li><Link className='link' to='/logout'>Logout</Link></li>
+        <li><Link className='link' to='/create-work-center'>Create work center</Link></li>
       </ul>
     </nav>
   );
