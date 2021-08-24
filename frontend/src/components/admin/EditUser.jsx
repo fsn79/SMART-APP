98d0f461380ae315776d6f1e0e64b51618478b49
Alice <andreevaalisa42@gmail.com>
2021-08-24 14:13:27 +0300

    fixes frontend

--- frontend/src/components/admin/EditUser.jsx
+++ frontend/src/components/admin/EditUser.jsx
@@ -22,7 +22,7 @@ function EditUser({ user, setActive }) {
   return (
     <div id='editUserModal' className="modal_content flex-direction--column formbg padding-horizontal--48">
       {/* <button className="modal_close" onClick={() => setActive(false)}>&times;</button> */}
-      <img alt="close" src={closeIcon} onClick={() => setActive(false)} />
+      <img className="modal_close" alt="close" src={closeIcon} onClick={() => setActive(false)} />
       <span id="form-header" className="padding-bottom--15">Edit employee</span>
       <form id="editUser" onSubmit={handleSubmitEditUser}>
         <div className="field padding-bottom--24">
