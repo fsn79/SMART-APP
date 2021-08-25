4dafe4bffcfebcd499939eb838d5f84dad4f33b9
Miroslav <gmpPB@yandex.ru>
2021-08-25 20:18:09 +0300

    i18next part 2

--- frontend/src/components/admin/EditWorkCenter.jsx
+++ frontend/src/components/admin/EditWorkCenter.jsx
@@ -1,7 +1,9 @@
 import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import closeIcon from '../../images/closeicon.svg';
 
 function EditWorkCenter({ center, setActive }) {
+  const [t] = useTranslation('global');
   const dispatch = useDispatch();
   function handleSubmitEditCenter(e) {
     e.preventDefault();
@@ -18,29 +20,29 @@ function EditWorkCenter({ center, setActive }) {
   return (
     <div id='editWCModal' className="modal_content flex-direction--column formbg padding-horizontal--48">
     <img className="modal_close_button" alt="close" src={closeIcon} onClick={() => setActive(false)} />
-    <span className="padding-bottom--15" id="form-header">Edit Work Center</span>
+    <span className="padding-bottom--15" id="form-header">{t('editWorkCenter.title')}</span>
 <form onSubmit={handleSubmitEditCenter}>
    <div className="field padding-bottom--24">
-    <label htmlFor="centerName">Edit Work Center Name</label>
+    <label htmlFor="centerName">{t('editWorkCenter.eCenterName')}</label>
     <input type="text" name="centerName" defaultValue={center.name} autoFocus/>
   </div>
   <div className="field padding-bottom--24">
-    <label htmlFor="code">Edit Work Center code</label>
+    <label htmlFor="code">{t('editWorkCenter.eCode')}</label>
     <input type="text" name="code" defaultValue={center.code} />
   </div>
   <div className="field padding-bottom--24">
-    <label htmlFor="capacity">Edit Work Center capacity</label>
+    <label htmlFor="capacity">{t('editWorkCenter.eCapacity')}</label>
     <input type="number" step='0.01' name="capacity" defaultValue={center.capacity} />
   </div>
   <div className="field padding-bottom--24">
-  <label htmlFor="status">Edit Status</label>
+  <label htmlFor="status">{t('editWorkCenter.eStatus')}</label>
   <p><select name="status" className="selectStatus">
-        <option>Active</option>
-        <option>Disabled</option>
+        <option>{t('editWorkCenter.oActive')}</option>
+        <option>{t('editWorkCenter.oDisabled')}</option>
        </select></p>
   </div>
   <div className="field padding-bottom--24">
-    <input type="submit" name="submit" value="Edit" />
+    <input type="submit" name="submit" value={t('editWorkCenter.btn-edit')} />
   </div>
 </form>
 </div>
