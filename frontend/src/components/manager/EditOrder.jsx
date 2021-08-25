27d69a5f05a65498f59f2c3932ba0c76b8804a6b
Alice <andreevaalisa42@gmail.com>
2021-08-25 10:29:10 +0300

    styles

--- frontend/src/components/manager/EditOrder.jsx
+++ frontend/src/components/manager/EditOrder.jsx
@@ -17,24 +17,26 @@ function EditOrder({ order, setActive }) {
   return (
     <div id='editOrderModal' className="modal_content flex-direction--column formbg padding-horizontal--48">
       <img className="modal_close" alt="close" src={closeIcon} onClick={() => setActive(false)} />
-      <span className="padding-bottom--15">Edit Work Order</span>
+      <span id="form-header" className="padding-bottom--15">Edit Work Order</span>
       <form id="editOrder" onSubmit={handleSubmitEditOrder}>
         <div className="field padding-bottom--24">
           <label htmlFor="plannedQuantity">Edit Work Order planned quantity</label>
           <input type="number" step='1' name="plannedQuantity" defaultValue={order.quantity} autoFocus/>
         </div>
         <div className="field padding-bottom--24">
-          <div className="grid">
+          {/* <div className="grid"> */}
             <label htmlFor="promisedDate">Edit Work Order promised date</label>
-          </div>
+          {/* </div> */}
           <input type="date" name="promisedDate" defaultValue={order.promiseddate}/>
         </div>
+        <div className="field padding-bottom--24">
         <label htmlFor="productionPrioroty">Edit Production Prioroty</label>
         <p><select className="selectEditUser" name="prioroty">
           <option>Low</option>
           <option>Medium</option>
           <option>High</option>
         </select></p>
+        </div>
         <div className="field padding-bottom--24">
           <input type="submit" name="submit" value="Edit" />
         </div>
